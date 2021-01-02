2fc50ab3c4815d0500e18f789ad8670e8b617228
Bob "Wombat" Hogg <wombat@rwhogg.site>
2021-01-02 12:09:19 -0500

    Try that

--- Rakefile.macos.rb
+++ Rakefile.macos.rb
@@ -1,7 +1,8 @@
 task default: %W[bundle]
 
-task bundle: ["dist/What The Hex.app"]
+task bundle: ["dist/What The Hex.zip"]
 
 %W[game.py].each do |pyfile|
     sh "pyinstaller --noconsole --icon icon.ico --onefile #{pyfile} -n \"What The Hex\""
+    sh "7z a \"dist/What The Hex.zip\" \"dist/What The Hex.app\""
 end
