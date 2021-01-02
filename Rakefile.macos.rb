669dd499fe9dc8bb1a29bb305c84c366007e75e1
Bob "Wombat" Hogg <wombat@rwhogg.site>
2021-01-02 18:46:46 -0500

    wip

--- Rakefile.macos.rb
+++
@@ -1,8 +0,0 @@
-task default: %W[bundle]
-
-task bundle: ["dist/What The Hex.zip"]
-
-%W[game.py].each do |pyfile|
-    sh "pyinstaller --noconsole --icon icon.png --add-data=\"README.md:.\" --add-data=\"*.gif:.\" --add-data=\"*.wav:.\" --add-data=\"*.ogg:.\" --add-data=\"*.png:.\" --add-data=\"*.ttf:.\" --onefile #{pyfile} -n \"What The Hex\""
-    sh "7z a \"dist/What The Hex.zip\" \"dist/What The Hex.app\""
-end
