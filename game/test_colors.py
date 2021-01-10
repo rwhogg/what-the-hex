cdc1c9e9985ff0207fb89628384374f7458458fc
Bob "Wombat" Hogg <wombat@rwhogg.site>
2021-01-09 20:08:34 -0500

    Fix indent

--- game/test_colors.py
+++ game/test_colors.py
@@ -14,7 +14,6 @@
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-
 import unittest
 
 import pygame
@@ -23,7 +22,12 @@ import colors
 
 
 def test_colors():
-    assert colors.EDGE_COLOR_OPTIONS == [pygame.Color(0x7c, 0xfc, 0), pygame.Color(0x4b, 0, 0x82), pygame.Color(255, 0x14, 0x93), pygame.Color(255, 0xd7, 0)]
+    assert colors.EDGE_COLOR_OPTIONS == [
+        pygame.Color(0x7c, 0xfc, 0),
+        pygame.Color(0x4b, 0, 0x82),
+        pygame.Color(255, 0x14, 0x93),
+        pygame.Color(255, 0xd7, 0)
+    ]
 
 
 if __name__ == '__main__':
