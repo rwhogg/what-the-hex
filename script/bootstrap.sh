454cfdb0f1908e6e3e864896c1a42f003be1de6b
Bob "Wombat" Hogg <wombat@rwhogg.site>
2021-01-09 20:58:43 -0500

    Ugh

--- script/bootstrap.sh
+++ script/bootstrap.sh
@@ -2,3 +2,7 @@
 
 pip install briefcase
 pip install flake8 mypy nose pylint yapf
+
+# This should install from pyproject.toml,
+# but until https://github.com/pypa/pip/issues/8049 is resolved, not happening...
+pip install pygame
