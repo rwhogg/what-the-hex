202e2dbe88df8dd9f444d4c686ade04d344c5fe6
Bob "Wombat" Hogg <wombat@rwhogg.site>
2021-01-21 21:00:55 -0500

    Oops

--- game/__main__.py
+++ game/__main__.py
@@ -23,7 +23,6 @@ import toga
 
 logging.basicConfig(filename=os.path.join(str(pathlib.Path.home()), ".what-the-hex.log"), level=logging.INFO)
 
-
 # Note, I think Toga is actually running as the main package in Briefcase,
 # so this import style is necessary.
 try:
