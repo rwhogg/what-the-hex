2833d7560ba8e6b4bc01cb71cb973066a51067e7
Bob "Wombat" Hogg <wombat@rwhogg.site>
2021-01-25 21:33:11 -0500

    Oops

--- script/bootstrap.sh
+++ script/bootstrap.sh
@@ -1,15 +1,10 @@
 #!/usr/bin/env bash
 
-<<<<<<< HEAD
 pip install briefcase
-pip install flake8 nose pylint  yapf
+pip install flake8 nose pylint yapf
 
-# FIXME: see https://github.com/nexB/scancode-toolkit/issues/2368
-pip install commoncode==20.10.20
-
-=======
 pip install -r requirements.txt
->>>>>>> 77d2125 (Snyk readyness)
+
 if [ -x "$(which npm)" ]; then
     npm i -g pyright
 fi
