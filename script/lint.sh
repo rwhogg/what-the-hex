adac3d2aeb41e6654a2165c3c71fc260b2900971
Bob "Wombat" Hogg <wombat@rwhogg.site>
2021-07-31 10:09:27 -0400

    Ignore false positive in gendarme

--- script/lint.sh
+++ script/lint.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
 if [ -r project/.mono/temp/bin/Debug/WhatTheHex.dll ]; then
-    gendarme project/.mono/temp/bin/Debug/WhatTheHex.dll -html gendarme.html --config rules.xml --set WhatTheHex
+    gendarme project/.mono/temp/bin/Debug/WhatTheHex.dll \
+        --html gendarme.html \
+        --config rules.xml \
+        --ignore gendarme.ignore \
+        --set WhatTheHex
 else
     echo "Please compile the assemblies!"
     exit 1
