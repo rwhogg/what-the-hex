e31569b392d1d469db6a0d9b74789d6ceab4aa4d
Bob "Wombat" Hogg <wombat@rwhogg.site>
2021-08-15 17:23:16 -0400

    Prevent use of the mouse wheel

--- project/Grid.cs
+++ project/Grid.cs
@@ -165,8 +165,8 @@ public class Grid : Node2D
 
     private void HandleMouseClick(InputEventMouseButton eventMouseButton)
     {
-        // ensure we don't double-rotate from a single click
-        if(eventMouseButton.IsPressed())
+        // ensure we don't double-rotate from a single click and that we don't accidentally trigger from the mouse wheel
+        if(eventMouseButton.IsPressed() || eventMouseButton.ButtonIndex >= (int) ButtonList.Middle)
         {
             return;
         }
