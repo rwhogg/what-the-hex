103f5a2b42a16d74e44d5415f9db335fa914a1b4
Bob "Wombat" Hogg <wombat@rwhogg.site>
2021-10-16 16:29:56 -0400

    Refactor the Grid checking

--- project/GridUtils.cs
+++ project/GridUtils.cs
@@ -5,6 +5,14 @@ using Godot;
 
 class GridUtils
 {
+    enum Side
+    {
+        TL,
+        TR,
+        BL,
+        BR
+    }
+
     public static HashSet<Hexagon> CheckMatches(Hexagon[][] Array, Hexagon affectedHexagon, out Godot.Collections.Dictionary<Color, int> matchedColors, int[] HexagonsPerRow)
     {
         HashSet<Hexagon> matchedHexagons = new HashSet<Hexagon>();
@@ -16,9 +24,7 @@ class GridUtils
         int row = (int)affectedHexagon.I;
         int column = (int)affectedHexagon.J;
 
-        // FIXME: 2 issues here
-        // 1. This logic might have to be adjusted to handle layouts with unequal columns per row
-        // 2. More generally, look into refactoring this (in trunk). Maybe the checking logic can be abstracted?
+        // FIXME: this logic might have to be adjusted to handle layouts with unequal columns per row
 
         // top-left
         if(row != 0 && column != 0)
@@ -26,10 +32,7 @@ class GridUtils
             Hexagon hexNW = Array[row - 1][column - 1];
             Hexagon hexN = Array[row - 1][column];
             Hexagon hexW = Array[row][column - 1];
-            bool nwEq = affectedHexagon.EdgeColors[tle] == hexNW.EdgeColors[bre];
-            bool nEq = affectedHexagon.EdgeColors[tle] == hexN.EdgeColors[ble];
-            bool wEq = affectedHexagon.EdgeColors[tle] == hexW.EdgeColors[tre];
-            if(nwEq && nEq && wEq)
+            if(CheckSide(affectedHexagon, Side.TL, Array))
             {
                 matchedHexagons.UnionWith(new Hexagon[] { affectedHexagon, hexNW, hexN, hexW });
                 // No need to += 1 here because this is definitely the first instance
@@ -42,10 +45,7 @@ class GridUtils
             Hexagon hexNE = Array[row - 1][column + 1];
             Hexagon hexN = Array[row - 1][column];
             Hexagon hexE = Array[row][column + 1];
-            bool neEq = affectedHexagon.EdgeColors[tre] == hexNE.EdgeColors[ble];
-            bool nEq = affectedHexagon.EdgeColors[tre] == hexN.EdgeColors[bre];
-            bool eEq = affectedHexagon.EdgeColors[tre] == hexE.EdgeColors[tle];
-            if(neEq && nEq && eEq)
+            if(CheckSide(affectedHexagon, Side.TR, Array))
             {
                 matchedHexagons.UnionWith(new Hexagon[] { affectedHexagon, hexNE, hexN, hexE });
                 if(matchedColors.ContainsKey(affectedHexagon.EdgeColors[tre]))
@@ -65,10 +65,7 @@ class GridUtils
             Hexagon hexSE = Array[row + 1][column + 1];
             Hexagon hexS = Array[row + 1][column];
             Hexagon hexE = Array[row][column + 1];
-            bool seEq = affectedHexagon.EdgeColors[bre] == hexSE.EdgeColors[tle];
-            bool sEq = affectedHexagon.EdgeColors[bre] == hexS.EdgeColors[tre];
-            bool eEq = affectedHexagon.EdgeColors[bre] == hexE.EdgeColors[ble];
-            if(seEq && sEq && eEq)
+            if(CheckSide(affectedHexagon, Side.BR, Array))
             {
                 matchedHexagons.UnionWith(new Hexagon[] { affectedHexagon, hexSE, hexS, hexE });
                 if(matchedColors.ContainsKey(affectedHexagon.EdgeColors[bre]))
@@ -88,10 +85,7 @@ class GridUtils
             Hexagon hexSW = Array[row + 1][column - 1];
             Hexagon hexS = Array[row + 1][column];
             Hexagon hexW = Array[row][column - 1];
-            bool swEq = affectedHexagon.EdgeColors[ble] == hexSW.EdgeColors[tre];
-            bool sEq = affectedHexagon.EdgeColors[ble] == hexS.EdgeColors[tle];
-            bool wEq = affectedHexagon.EdgeColors[ble] == hexW.EdgeColors[bre];
-            if(swEq && sEq && wEq)
+            if(CheckSide(affectedHexagon, Side.BL, Array))
             {
                 matchedHexagons.UnionWith(new Hexagon[] { affectedHexagon, hexSW, hexS, hexW });
                 if(matchedColors.ContainsKey(affectedHexagon.EdgeColors[ble]))
@@ -106,5 +100,62 @@ class GridUtils
         }
 
         return matchedHexagons;
+    }
+
+    private static bool CheckSide(Hexagon affectedHexagon, Side side, Hexagon[][] Array)
+    {
+        int tre = 0;
+        int bre = 1;
+        int ble = 3;
+        int tle = 4;
+        int row = (int)affectedHexagon.I;
+        int column = (int)affectedHexagon.J;
+
+        int[] n1;
+        int[] n2;
+        int[] n3;
+        int[] edgesToCheck;
+        int originalEdge;
+
+        switch(side)
+        {
+            case Side.TL:
+                n1 = new int[]{row - 1, column - 1};
+                n2 = new int[]{row - 1, column};
+                n3 = new int[]{row, column - 1};
+                edgesToCheck = new int[]{bre, ble, tre};
+                originalEdge = tle;
+                break;
+            case Side.TR:
+                n1 = new int[]{row - 1, column + 1};
+                n2 = new int[]{row - 1, column};
+                n3 = new int[]{row, column + 1};
+                edgesToCheck = new int[]{ble, bre, tle};
+                originalEdge = tre;
+                break;
+            case Side.BR:
+                n1 = new int[]{row + 1, column + 1};
+                n2 = new int[]{row + 1, column};
+                n3 = new int[]{row, column + 1};
+                edgesToCheck = new int[]{tle, tre, ble};
+                originalEdge = bre;
+                break;
+            case Side.BL:
+                n1 = new int[]{row + 1, column - 1};
+                n2 = new int[]{row + 1, column};
+                n3 = new int[]{row, column - 1};
+                edgesToCheck = new int[]{tre, tle, bre};
+                originalEdge = ble;
+                break;
+            default:
+                throw new Exception("BAD SIDE CHECK");
+        }
+
+        Color colorToCheck = affectedHexagon.EdgeColors[originalEdge];
+        bool n1eq = colorToCheck == Array[n1[0]][n1[1]].EdgeColors[edgesToCheck[0]];
+        bool n2eq = colorToCheck == Array[n2[0]][n2[1]].EdgeColors[edgesToCheck[1]];
+        bool n3eq = colorToCheck == Array[n3[0]][n3[1]].EdgeColors[edgesToCheck[2]];
+
+        return n1eq && n2eq && n3eq;
     }
 }
