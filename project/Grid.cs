dc7368a0a97bcbe27419d2ef67635b34934032f2
Bob "Wombat" Hogg <wombat@rwhogg.site>
2021-10-22 18:41:20 -0400

    Split out the input-receiving parts of the grid

--- project/Grid.cs
+++ project/Grid.cs
@@ -13,18 +13,15 @@
 // limitations under the License.
 
 using System;
-using System.Collections.Generic;
-using System.Globalization;
 
 using Godot;
 
 using static Godot.JoystickList;
-using static Godot.Mathf;
 
 /**
  * Wrapper class for a grid of hexagons
  */
-public class Grid : Node2D
+public class Grid : BaseGrid
 {
     // FIXME: the hexagon and the grid classes are so tightly intertwined, and also they
     // probably have game logic embedded in them. It would be better if I could
@@ -32,45 +29,21 @@ public class Grid : Node2D
     // emitted by the hexagons and grid as appropriate, with the game logic moved to the game
     // component in signal handlers
 
-    /**
-     * Signal issued when any hexagon in the grid is rotated
-     */
-    [Signal]
-    public delegate void HexagonRotated(Hexagon rotatedHexagon, Godot.Collections.Array matchedHexagons, Godot.Collections.Dictionary<Color, int> matchedColors);
-
-    // FIXME: this signal should definitely NOT be emitted by the grid, but we're doing most of the non-trivial button handling here for now, so it's gotta go here for now
-    [Signal]
-    public delegate void PowerUpActivated();
-
-    /**
-     * The underlying jagged array of hexagons
-     */
-    public Hexagon[][] Array;
-    // FIXME: Consider changing the field 'Array' to a private or internal field and add a 'SetArray' method.
-    // Would have to look into the specifics of how I would want to do that
-
     private readonly int[] HexagonsPerRow;
 
     private readonly Hexagon[] SelectedHexagons = new Hexagon[1 + Convert.ToInt32(RuntimeConfig.Is2Player)];
-
-    private static readonly Random Rand = new Random();
-
-    private static readonly CultureInfo culture = ConfigFileUtils.GetCulture();
-
     /**
      * Do not use. Only for constructing Mono temp objects
      */
-    public Grid() { }
+    public Grid() : base() { }
 
     /**
      * Starts off a grid at the specified start point.
      * @param startPoint Top-left point of the grid
      * @param hexagonsPerRow Number of hexagons per row
      */
-    public Grid(Vector2 startPoint, int[] hexagonsPerRow)
+    public Grid(Vector2 startPoint, int[] hexagonsPerRow) : base(startPoint, hexagonsPerRow)
     {
-        Position = startPoint;
-        HexagonsPerRow = hexagonsPerRow;
     }
 
     /**
@@ -193,102 +166,5 @@ public class Grid : Node2D
             direction = Direction.RIGHT;
         }
         HandleRotation(affectedHexagon, direction, 0);
-    }
-
-    /**
-     * Select the hexagon at the specified position
-     * @param i Row to select
-     * @param j Column to select
-     * @param playerIndex Currently acting player
-     */
-    public void SetSelectedHexagon(int i, int j, int playerIndex)
-    {
-        Array[i][j].Selected[playerIndex] = true;
-        SelectedHexagons[playerIndex] = Array[i][j];
-    }
-
-    /**
-     * Select a number of hexagons in the grid for later replacement
-     * @param numHexagonsToReplace The number of hexagons to select
-     */
-    public void SelectHexagonsForReplacement(int numHexagonsToReplace)
-    {
-        int n = 0;
-        while(n < numHexagonsToReplace)
-        {
-#pragma warning disable CA5394
-            int randomRow = Rand.Next(0, HexagonsPerRow.Length);
-            int randomCol = Rand.Next(0, HexagonsPerRow[randomRow]);
-#pragma warning restore CA5394
-            if(Array[randomRow][randomCol].MarkedForReplacement)
-            {
-                continue;
-            }
-            Array[randomRow][randomCol].StartRefreshTimer();
-            n++;
-        }
-    }
-
-    /**
-     * Force the currently selected hexagon to rotate
-     * @param direction Which direction to rotate
-     * @param playerIndex Currently active player
-     */
-    public void RotateSelected(Direction direction, int playerIndex)
-    {
-        HandleRotation(SelectedHexagons[playerIndex], direction, playerIndex);
-    }
-
-    private void HandleRotation(Hexagon affectedHexagon, Direction direction, int playerIndex)
-    {
-        affectedHexagon.Rot(direction);
-        Color colorToFlash = Hexagon.DefaultHexColor;
-        HashSet<Hexagon> matchedHexagons = GridUtils.CheckMatches(Array, affectedHexagon, out Godot.Collections.Dictionary<Color, int> matchedColors, HexagonsPerRow);
-        if(matchedColors.Keys.Count > 0)
-        {
-            foreach(var pair in matchedColors)
-            {
-                colorToFlash = pair.Key;
-            }
-        }
-        Hexagon[] hexagons = new Hexagon[matchedHexagons.Count];
-        int i = 0;
-        foreach(Hexagon hexagon in matchedHexagons)
-        {
-            if(OS.IsDebugBuild())
-            {
-                GD.Print("Matched hex " + hexagon.I.ToString(culture) + ", " + hexagon.J.ToString(culture));
-            }
-            hexagon.Matched = true;
-            hexagon.AbortReplacement();
-            hexagon.FlashColor(colorToFlash);
-            hexagon.Refresh();
-            hexagons[i++] = hexagon;
-        }
-        EmitSignal(nameof(HexagonRotated), affectedHexagon, hexagons, matchedColors, playerIndex);
-    }
-
-    private void HandlePowerUp()
-    {
-        // FIXME: the grid really should not be handling button presses
-        // not to mention that this signal is definitely not something it should be handling either
-        EmitSignal(nameof(PowerUpActivated));
-    }
-
-    private Hexagon GetAffectedHexagon(Vector2 clickPos)
-    {
-        int j = RoundToInt(clickPos.x / (2 * Hexagon.BigRadius() + 0.2F * Hexagon.EdgeThickness));
-        int i = RoundToInt(clickPos.y / (2 * Hexagon.SmallRadius() + 0.8F * Hexagon.EdgeThickness));
-        if(i >= HexagonsPerRow.Length || j >= HexagonsPerRow[i])
-        {
-            if(OS.IsDebugBuild())
-            {
-                GD.Print("Bad Click! i, j: " + i.ToString(culture) + ", " + j.ToString(culture));
-                GD.Print("Click Position is " + clickPos);
-            }
-            return null;
-        }
-
-        return Array[i][j];
     }
 }
