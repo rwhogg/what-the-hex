a260e41ff541b4dd0dcfe9c19df4bfeb6948e6b8
Bob "Wombat" Hogg <wombat@rwhogg.site>
2021-11-25 11:57:34 -0500

    Cleanup

--- script/precommit.sh
+++ script/precommit.sh
@@ -2,9 +2,14 @@
 
 pushd project || exit
 
-dotnet format "WhatTheHex.csproj"
+PROJ="WhatTheHex.csproj"
+
+dotnet format whitespace $PROJ
+dotnet format style $PROJ
+dotnet format analyzers $PROJ
 
 # Add license and address overzealous HTML-escaping of my name
+# FIXME: replace with a dotnet file header format requirement
 addlicense -c 'Bob "Wombat" Hogg' -l apache ./*.cs
 git status | grep modified | grep -v precommit.sh | awk '{print $2}' | xargs sed -i 's/&#34;Wombat&#34;/"Wombat"/g'
 
