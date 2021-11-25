1efc12be1c09a97ce6fa7f10efe6aedf8218af27
Bob "Wombat" Hogg <wombat@rwhogg.site>
2021-11-25 12:44:56 -0500

    Closer...

--- script/precommit.sh
+++ script/precommit.sh
@@ -10,7 +10,7 @@ dotnet format analyzers $PROJ
 
 # Address overzealous HTML-escaping of my name and handle the fact that semicolons indicate comments in Editorconfig files
 git status | grep modified | grep -v precommit.sh | awk '{print $2}' | xargs sed -i 's/&#34;Wombat&#34;/"Wombat"/g'
-git status | grep modified | grep -v precommit.sh | awk '{print $2}' | xargs sed -i 's/License")/License");/g'
+git status | grep modified | grep -v precommit.sh | grep -v .editorconfig | awk '{print $2}' | xargs sed -i 's/License")/License");/g'
 
 ../script/format.sh
 
